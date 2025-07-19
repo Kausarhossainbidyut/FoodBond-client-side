945f41a7d410d06a1c2f9e91098fd0178d293d18
Kausar Hossain Bidyut <khbidyut31@gmail.com>
2025-07-19 22:55:10 +0600

    error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,6 @@
 import { useRouteError } from "react-router";
 import Header from "../components/Header";
+import { NavLink } from 'react-router';
 
 const Error = () => {
   const error = useRouteError();
@@ -7,7 +8,16 @@ const Error = () => {
   return (
     <div>
       <Header></Header>
-      <div className="flex min-h-screen items-center justify-center">Error</div>
+      <div className='flex justify-center items-center mt-[50px] '>
+        <div>
+          <div>
+            <img className='w-[600px] h-[400px] rounded-2xl' src="https://i.ibb.co.com/8ncBh8YS/images.png" alt="error image" />
+          </div>
+          <div className='text-center'>
+            <NavLink to={'/'} className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl bg-[#a8de57] hover:bg-[#7bbd19] text-blue-700 hover:text-blue-100 font-bold rounded-2xl underline mt-[30px]">Go Home</NavLink>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
